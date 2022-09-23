1a410ef75f5240df771075394a04582b1ea6be14
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2022-09-23 17:45:15 +0300

    default homepage

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (<>
-  <h1 className='text-3xl font-bold underline'>Hello world</h1>
+  <h1 className='text-3xl text-center'>Discord Bot Panel</h1>
+
+  <div className='flex justify-center'>
+    <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Open Panel</a>
+  </div>
   </>)
 }
