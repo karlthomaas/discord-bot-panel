a6d25e2f6e94f861e5b2f552b9a268fe3fb214a9
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2022-09-23 17:44:07 +0300

    Allowed guild images from Discord

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        pathname:'/icons/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
