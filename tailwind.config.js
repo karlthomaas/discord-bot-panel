477e401a9f8d868a24a8f7033e8eded96f7dbe78
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2022-09-23 17:44:27 +0300

    Added default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          900: '#202225',
+          400: '#36393f'
+        }
+      }
+    },
   },
   plugins: [],
 }
