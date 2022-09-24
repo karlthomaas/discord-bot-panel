cf0b8ba500b9549e7d6b90ab4a140757b7e61240
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2022-09-24 15:33:15 +0300

    Guilds

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,4 @@
-import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
-import { FaFire, FaPoo } from 'react-icons/fa';
-import { HiOutlineQuestionMarkCircle } from "react-icons/hi";
 import { FiMessageCircle } from "react-icons/fi";
-
 import Image from 'next/image'
 
 const Sidebar = ({ guilds }) => {
@@ -11,15 +7,15 @@ const Sidebar = ({ guilds }) => {
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
             { guilds.map((data, id: number) => {
                 const guild_icon = data.icon == null ? null: `https://cdn.discordapp.com/icons/${data.id}/${data.icon}.webp?size=96`
-                return(<SideBarIcon key={id} guild_icon={guild_icon} name={data.name}/>)})
+                return(<SideBarIcon key={id} guild_icon={guild_icon} name={data.name} guild_id={undefined}/>)})
             }
         </div>
     )
 }
 
-const SideBarIcon = ({ name, guild_icon}) => (
+const SideBarIcon = ({ name, guild_icon, guild_id}) => (
     <div className="sidebar-icon group">
-    {guild_icon == null ? <FiMessageCircle size="20"/>: <Image className="rounded-full" src={guild_icon} alt={name} width={100} height={100}/>}
+    {guild_icon == null ? <FiMessageCircle size="20"/>: <a href={process.env.NEXT_PUBLIC_WEBPAGE_BASE_URL + `/panel/${guild_id}/`} ><Image className="sidebar-icon" src={guild_icon} alt={name} width={100} height={100}/></a>}
       <span className="sidebar-tooltip group-hover:scale-100">
         {name}
       </span>
