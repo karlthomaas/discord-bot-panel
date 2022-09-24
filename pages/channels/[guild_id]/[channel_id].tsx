f768dff1d76d0eb96c318cc9ffebf82abfd74f90
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2022-09-24 15:34:24 +0300

    Endpoints for viewing channels/texts

---
+++ pages/channels/[guild_id]/[channel_id].tsx
@@ -0,0 +1,3 @@
+export default function UserPanel({ guild, channel}){
+    return (<h1>test</h1>)
+}
---
+++ pages/channels/index.tsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+
+export default function UserPanel({ guilds }){
+    return (<h1>test AAA</h1>)
+}
+
+
+export async function getServerSideProps({ pageProps, context }){
+    const guilds = 1
+    // const guilds = context.ctx.guilds
+    console.log(pageProps)
+
+    return {props: { guilds }}
+}
