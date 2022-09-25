0d347f4188c0d35ce5ad7c5e9d0cfe3bb706a5f7
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2022-09-25 20:22:37 +0300

    Adjustment

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,7 @@ import { FiMessageCircle } from "react-icons/fi";
 import Image from 'next/image'
 
 const Sidebar = ({ guilds, changeGuild }) => {
+
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
             { guilds.map((data, id: number) => {
@@ -15,8 +16,8 @@ const Sidebar = ({ guilds, changeGuild }) => {
 const SideBarIcon = ({ guild_icon, data, changeGuild}) => {
 
   return (
-    <div className="sidebar-icon group">
-    {guild_icon == null ? <FiMessageCircle size="20"/>: <a onClick={() => changeGuild(data)}><Image className="sidebar-icon" src={guild_icon} alt={data.name} width={100} height={100}/></a>}
+    <div className="sidebar-group">
+    {guild_icon == null ? <div className="sidebar-icon"><FiMessageCircle size="20"/></div>: <a onClick={() => changeGuild(data)}><Image className="sidebar-icon" src={guild_icon} alt={data.name} width={50} height={50}/></a>}
       <span className="sidebar-tooltip group-hover:scale-100">
         {data.name}
       </span>
