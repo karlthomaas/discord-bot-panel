13b9fb796ae27df74538bc4e41aeee8e033b3af4
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2022-09-25 20:54:17 +0300

    Added a new colour and create better looking homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (<>
-  <h1 className='text-3xl text-center'>Discord Bot Panel</h1>
+  <style jsx global>{`
+  body {
+    background: #5865F2;
+  }
+`}</style>
+
+  <h1 className='text-3xl text-center text-white my-10'>Discord Bot Panel</h1>
 
   <div className='flex justify-center'>
-    <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Open Panel</a>
+    <Link href="/panel"><a className="bg-indigo-50 text-white font-bold py-2 px-4 rounded-full">Open Panel</a></Link>
+    
   </div>
   </>)
 }
