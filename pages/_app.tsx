5774eb0b6aa73ab4be0fa0d918ea2a72df857372
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2022-09-27 00:09:48 +0300

    viewport update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+  <Head>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+  </Head>
   return <Component {...pageProps} />
 }
 
