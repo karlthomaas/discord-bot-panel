0fe46b1132939c40841811a8f99e05b0c950d867
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2022-09-28 19:33:28 +0300

    Server / Channel info design

--- components/info.tsx
+++ components/info.tsx
@@ -3,13 +3,13 @@ const Serverinfo = ({currentGuild, currentChannel}) => {
     console.log("🚀 ~ file: info.tsx ~ line 2 ~ Serverinfo ~ currentGuild", currentGuild)
 
     if (currentChannel.response.length !== 0){
-        var css= "mt-0"
+        var css= "w-56"
         var channel_jsx = (
                 <ul>
                     <li className={css}>Name: {currentChannel.response.name}</li>
-                    <li>ID: {currentChannel.response.id}</li>
-                    <li>Topic: {currentChannel.response.topic == undefined ? "Not set": currentChannel.response.topic}</li>
-                    <li>NSFW: {currentChannel.response.nsfw == true ? "True": "False"}</li>
+                    <li className={css}>ID: {currentChannel.response.id}</li>
+                    <li className={css}>Topic: {currentChannel.response.topic == undefined ? "Not set": currentChannel.response.topic}</li>
+                    <li className={css}>NSFW: {currentChannel.response.nsfw == true ? "True": "False"}</li>
                 </ul>)
 
     } else {
@@ -18,7 +18,7 @@ const Serverinfo = ({currentGuild, currentChannel}) => {
 
     if (currentGuild !== undefined){
         var guild_jsx =(
-            <ul>
+            <ul className="">
                 <li>Name: {currentGuild.name}</li>
                 <li>ID: {currentGuild.id}</li>
             </ul>
@@ -29,12 +29,12 @@ const Serverinfo = ({currentGuild, currentChannel}) => {
 
     return (
         <div className="server-info-bar ">
-            <div className="mt-3 ml-3">
-                <h1>Current Guild:</h1>
+            <div className="mt-3 ml-3 break-all">
+                <h1>Guild:</h1>
                 {guild_jsx}
             </div>
-            <div className="mt-3 ml-3">
-                <h1>Current Channel:</h1>
+            <div className="mt-3 ml-3 text-sm">
+                <h1>Channel:</h1>
                 {channel_jsx}
             </div>
             
@@ -42,4 +42,4 @@ const Serverinfo = ({currentGuild, currentChannel}) => {
     )
 }
 
-export default Serverinfo;
+export default Serverinfo; 
